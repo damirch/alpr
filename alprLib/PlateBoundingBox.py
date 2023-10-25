aade6688d398cd0526798adf40c87249138212bb
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2023-10-25 10:41:20 +0200

    show bounding box on image

---
+++ alprLib/PlateBoundingBox.py
@@ -0,0 +1,30 @@
+import xml.etree.cElementTree as ET
+
+class PlateBoundingBox:
+    def __init__(self, xml_path: str):
+        self.xml_path = xml_path
+        self.tree = ET.parse(xml_path)
+        self.root = self.tree.getroot()
+        
+        bbox = self.root.find('object').find('bndbox')
+        self.xmin = int(bbox.find('xmin').text)
+        self.ymin = int(bbox.find('ymin').text)
+        self.xmax = int(bbox.find('xmax').text)
+        self.ymax = int(bbox.find('ymax').text)
+        self.width = self.xmax - self.xmin
+        self.height = self.ymax - self.ymin
+        self.center = (self.xmin + self.width/2, self.ymin + self.height/2)
+        self.area = self.width * self.height
+
+    def describe(self):
+        print("PlateBoundingBox")
+        print("xml_path: {}".format(self.xml_path))
+        print("xmin: {}".format(self.xmin))
+        print("ymin: {}".format(self.ymin))
+        print("xmax: {}".format(self.xmax))
+        print("ymax: {}".format(self.ymax))
+        print("width: {}".format(self.width))
+        print("height: {}".format(self.height))
+        print("center: {}".format(self.center))
+        print("area: {}".format(self.area))
+        print("")
