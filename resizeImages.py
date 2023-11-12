f9b72290c115d29ba7ab484269d74c5bc0edc018
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2023-11-12 17:57:02 +0100

    added image resizing script

---
+++ resizeImages.py
@@ -0,0 +1,41 @@
+# perform the following using python3
+#mkdir ./archive/images_resized
+#mogrify -resize 1280x720 ./archive/images/* -path ./archive/images_resized -monitor
+
+from PIL import Image
+import os
+import numpy as np
+
+# Input and output directories
+input_dir = './archive/images/'
+output_dir = './archive/images_resized/'
+
+# Desired dimensions
+new_width, new_height = 1280, 720
+
+# Ensure output directory exists
+os.makedirs(output_dir, exist_ok=True)
+
+# List all files in the input directory
+files = os.listdir(input_dir)
+
+for file in files:
+    if file.endswith('.jpg') or file.endswith('.png') or file.endswith('.jpeg'):
+        try:
+            # Open image
+            img = Image.open(os.path.join(input_dir, file))
+
+            # Resize using PIL
+            resized_img = img.resize((new_width, new_height))
+
+            # Convert to numpy array for saving
+            img_array = np.array(resized_img)
+
+            # Save the resized image
+            output_path = os.path.join(output_dir, file)
+            Image.fromarray(img_array).save(output_path)
+
+            print(f"Resized {file} and saved to {output_path}")
+
+        except Exception as e:
+            print(f"Failed to process {file}: {e}")
--- resizeImages.sh
+++
@@ -1,2 +0,0 @@
-mkdir ./archive/images_resized
-mogrify -resize 1280x720 ./archive/images/* -path ./archive/images_resized
