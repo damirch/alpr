946e7a54ffeb7b48323f7385d343dd5bf2341e39
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2023-12-12 17:25:27 +0100

    write text a bit higher

--- alprLib/randomImage.py
+++ alprLib/randomImage.py
@@ -33,8 +33,8 @@ if __name__ == "__main__":
         x, y, w, h = box
         plt.imshow(img[y:y+h, x:x+w, ::-1])
         plt.show()
-        if w > 70:
-            cv2.putText(img, reader.read(img[y:y+h, x:x+w]), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+        if w > 40:
+            cv2.putText(img, reader.read(img[y:y+h, x:x+w]), (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
         cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
     plt.imshow(img[:, :, ::-1])
